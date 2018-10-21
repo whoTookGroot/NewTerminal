368d6f8f0ba8291c7989a2d89701af43f7bc86d9
Erik Grootendorst <hotheadkettler@sbcglobal.net>
2018-10-20 18:18:52 -0700

    Initial Commit

---
+++ NewTerminal/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  NewTerminal
+//
+//  Created by Erik on 10/20/18.
+//  Copyright © 2018 Erik. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
